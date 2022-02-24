8cb9e0cea0a3df7e60f187f4c433cfeec97699b7
veronikerrr <vero_weber@gmx.at>
2022-02-24 19:37:11 +0100

    function for finding works in text cells

--- archiv/management/commands/link_works.py
+++ archiv/management/commands/link_works.py
@@ -10,4 +10,51 @@ class Command(BaseCommand):
     help = 'Links works to Events'
 
     def handle(self, *args, **kwargs):
-        print('hello')
+        import re
+
+        dict_lit = {}
+
+        all_events = Event.objects.all()
+
+        for event in all_events:
+        
+            lit_main_text = event.main_text
+            lit_notes_lit = event.notes_lit
+            lit_notes_img = event.notes_img
+            lit_notes_facs = event.notes_facs
+            lit_notes_archive = event.notes_archive
+            lit_notes_text = event.notes_text
+            lit_note = event.note
+        
+            number = []
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_main_text))
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_lit))
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_img))
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_facs))
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_archive))
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_text))
+            number.append(re.findall('([0-9]+)/[0-9]+', lit_note))
+            
+
+            if len(number) != 0:
+                                
+                for number_list in number:
+                    
+                    for literature in number_list:
+                        
+                        number_int = int(literature)
+                        lit_string = 'Lit.' + ('%04d' % number_int)
+                        
+                        try:
+                            related_work = Work.objects.get(order_code=lit_string)
+                        except: # DoesNotExist:
+                            continue
+
+                        event.work.add(related_work)
+
+                        if event.id in dict_lit:
+                            if lit_string not in dict_lit[event.id]:
+                                dict_lit[event.id].append(lit_string)
+
+                        else:
+                            dict_lit[event.id] = [lit_string]
