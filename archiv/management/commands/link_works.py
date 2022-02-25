675617a3669a6280261b5cb938cbbd0c4cf18458
csae8092 <peter.andorfer@oeaw.ac.at>
2022-02-25 10:22:55 +0100

    fix linting

--- archiv/management/commands/link_works.py
+++ archiv/management/commands/link_works.py
@@ -1,23 +1,20 @@
-
+import re
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
-from tqdm import tqdm
 
 from archiv.models import Event, Work
-import re
 
 
 class Command(BaseCommand):
     help = 'Links works to Events'
 
     def handle(self, *args, **kwargs):
-        import re
 
         dict_lit = {}
 
         all_events = Event.objects.all()
 
         for event in all_events:
-        
             lit_main_text = event.main_text
             lit_notes_lit = event.notes_lit
             lit_notes_img = event.notes_img
@@ -25,7 +22,7 @@ class Command(BaseCommand):
             lit_notes_archive = event.notes_archive
             lit_notes_text = event.notes_text
             lit_note = event.note
-        
+
             number = []
             number.append(re.findall('([0-9]+)/[0-9]+', lit_main_text))
             number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_lit))
@@ -34,20 +31,19 @@ class Command(BaseCommand):
             number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_archive))
             number.append(re.findall('([0-9]+)/[0-9]+', lit_notes_text))
             number.append(re.findall('([0-9]+)/[0-9]+', lit_note))
-            
 
             if len(number) != 0:
-                                
+
                 for number_list in number:
-                    
+
                     for literature in number_list:
-                        
+
                         number_int = int(literature)
                         lit_string = 'Lit.' + ('%04d' % number_int)
-                        
+
                         try:
                             related_work = Work.objects.get(order_code=lit_string)
-                        except: # DoesNotExist:
+                        except ObjectDoesNotExist:
                             continue
 
                         event.work.add(related_work)
