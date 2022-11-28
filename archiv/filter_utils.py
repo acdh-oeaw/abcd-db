3e5363c33c480422789cb646afde9b45cf2893a6
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-28 19:05:48 +0100

    fix: back to websearch #52

--- archiv/filter_utils.py
+++ archiv/filter_utils.py
@@ -5,8 +5,7 @@ from django.contrib.postgres.search import SearchQuery
 class SchrederFilter(FilterSet):
 
     def search_fulltext(self, queryset, field_name, value):
-        # query = SearchQuery(value, config="german", search_type="websearch") ToDo: uncomment after pg-server update
-        query = SearchQuery(value, config="german")
+        query = SearchQuery(value, config="german", search_type="websearch")
         qs = queryset.filter(vector_column=query)
 
         return qs
