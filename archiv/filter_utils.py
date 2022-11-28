820cd00096662c7e57723508ab19e30a2816e0fb
csae8092 <peter.andorfer@oeaw.ac.at>
2022-11-28 17:42:15 +0100

    hotfix: removed websearch feature

--- archiv/filter_utils.py
+++ archiv/filter_utils.py
@@ -5,7 +5,8 @@ from django.contrib.postgres.search import SearchQuery
 class SchrederFilter(FilterSet):
 
     def search_fulltext(self, queryset, field_name, value):
-        query = SearchQuery(value, config="german", search_type="websearch")
+        # query = SearchQuery(value, config="german", search_type="websearch") ToDo: uncomment after pg-server update
+        query = SearchQuery(value, config="german")
         qs = queryset.filter(vector_column=query)
 
         return qs
