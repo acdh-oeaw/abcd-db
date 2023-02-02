c23127c6664d0f0c4c95d21b8afd208f257f199e
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-02 12:18:30 +0100

    add `target="_blank"` on save to hrefs #79 * also added mgm-script to save all objects

---
+++ archiv/management/commands/save_all.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
+
+from archiv.models import Event, Person, Work
+
+
+class Command(BaseCommand):
+    help = 'Saves all Work, Persons and Events'
+
+    def handle(self, *args, **kwargs):
+        for item in [Work, Person, Event]:
+            objects = item.objects.all()
+            print(f"saving {objects.count()} {item}")
+            for x in tqdm(objects, total=objects.count()):
+                x.save()
+        print("Done saving")
---
+++ archiv/utils.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+
+
+def fix_hrefs(item):
+    fields = []
+    try:
+        fields = item.search_field_names()
+    except AttributeError:
+        pass
+    for x in fields:
+        data = getattr(item, x)
+        if data:
+            soup = BeautifulSoup(data, "html.parser")
+            for link in soup.find_all('a'):
+                href = link.get('href')
+                if href is not None:
+                    if "abcd.acdh" in href:
+                        pass
+                    else:
+                        link.attrs["target"] = "_blank"
+            setattr(item, x, str(soup))
+    return "done"
