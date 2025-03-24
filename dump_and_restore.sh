15b142b9e88fad6edf114fa53744247612fcb9d4
csae8092 <p.andorfer@gmail.com>
2025-03-24 11:44:55 +0100

    feat(data): import notebook [skip ci]

--- dump_and_restore.sh
+++ dump_and_restore.sh
@@ -1,3 +1,2 @@
-# rm abcd_db.sql
-# pg_dump -d abcd_db -h 127.0.0.1 -p 5433 -U abcd_db -c -f abcd_db.sql
-# psql -U postgres -d abcd_db < abcd_db.sql
+pg_dump -d abcd_db -h 127.0.0.1 -p 5433 -U abcd_db -c -f abcd_db.sql
+psql -U postgres -d abcd_db < abcd_db.sql
